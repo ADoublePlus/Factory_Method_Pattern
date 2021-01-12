dbf55a9c55df92193c136313479b6da68dd9cf84
John Wonson <jmpwonson@gmail.com>
2021-01-12 16:23:40 +1100

    Create IFactory.cs

---
+++ GOFFactoryMethod/GOFFactoryMethod/IFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace factory_method_pattern
+{
+    public interface IFactory
+    {
+        /// <summary>
+        /// Creates an instance of type TDestination from an instance of type TSource.
+        /// </summary>
+        /// <typeparam name="TSource">The type of object from which an instance of TDestination is to be created.</typeparam>
+        /// <typeparam name="TDestination">The type of object to be created.</typeparam>
+        /// <param name="source"></param>
+        /// <exception cref="CreatorNotFoundException">When no suitable creator is found.</exception>
+        /// <returns>An instance of type TDestination.</returns>
+        TDestination Create<TSource, TDestination>(TSource source)
+            where TSource : class
+            where TDestination : class;
+    }
+}
