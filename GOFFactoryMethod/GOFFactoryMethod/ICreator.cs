f485fd46d4abdd86afb54b411e4bd70b86f79f26
John Wonson <jmpwonson@gmail.com>
2021-01-12 16:18:24 +1100

    Create ICreator.cs

---
+++ GOFFactoryMethod/GOFFactoryMethod/ICreator.cs
@@ -0,0 +1,9 @@
+﻿namespace factory_method_pattern
+{
+    public interface ICreator<in TSource, out TDestination>
+        where TSource : class
+        where TDestination : class
+    {
+        TDestination Create(TSource source);
+    }
+}
